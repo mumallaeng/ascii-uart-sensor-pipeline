70481b2853eabff0c74fcee88ff195cfc0147cbb
jimin-max <jimin040219@gmail.com>
2026-05-02 15:02:28 +0900

    온습도센서 기능 구현 및 FND 연결

--- ascii_uart_sensor_pipeline.srcs/sim_1/new/tb_dht11_jm.v
+++ ascii_uart_sensor_pipeline.srcs/sim_1/new/tb_dht11_jm.v
@@ -1,5 +1,5 @@
 `timescale 1ns / 1ps
-module tb_dht11 ();
+module tb_dht11_jm ();
     // 시뮬레이션 파라미터
     parameter [7:0] HUMI_INT = 8'd60;  // 습도 정수부
     parameter [7:0] TEMP_INT = 8'd25;  // 온도 정수부
@@ -7,50 +7,58 @@ module tb_dht11 ();
         HUMI_INT, 8'h00, TEMP_INT, 8'h00, HUMI_INT + TEMP_INT
     };
     reg clk;
-    reg reset;
-    reg btn_start;
-
-    // 
+    reg rst;
+    reg btn_R;
+    reg sw15;
     reg dht_sensor_data;
-    reg io_oe;
-
-    wire [5:0] led;
-    wire dht_io;
+    reg io_oe; // io_oe = 1 → testbench가 센서 역할을 하면서 dht11 선을 직접 제어
+               // io_oe = 0 → testbench는 dht11 선을 놓음, high-Z 상태
+    //wire [5:0] led;
+    wire [3:0] fnd_com;
+    wire [7:0] fnd_data;
+    wire dht11;
 
     //  tb io mode 변환.
-    assign dht_io = (io_oe) ? dht_sensor_data : 1'bz;
+    assign dht11 = (io_oe) ? dht_sensor_data : 1'bz; // 센서 입장에서 io를 제어해주는 것
 
     dht11 dut (
-        .clk(clk),
-        .rst(reset),
-        .dht11_start(btn_start),
-        .dht11(dht_io),
-        .humidity(),
-        .temperature(),
-        .valid()
-    );
+    .clk(clk),
+    .rst(rst),
+    .btn_R(btn_R),
+    .sw15(sw15),
+    .led(led),
+    .fnd_com(fnd_com),
+    .fnd_data(fnd_data),
+    .dht11(dht11)
+);
+
     always #5 clk = ~clk;
     integer i = 0;
 
     initial begin
         clk = 0;
-        reset = 1;
+        rst = 1;
         io_oe = 0;
-        btn_start = 0;
+        btn_R = 0;
+        sw15 = 0; // 온도
 
         #100;
-        reset = 0;
+        rst = 0;
         #100;
-        btn_start = 1;
+        btn_R = 1;
+        sw15 = 1; // 습도
+        #1_000_000;
+        btn_R = 0;
         #100;
-        btn_start = 0;
-        #100;
-        wait (!dht_io);
+        wait (!dht11);
         // 18msec 대기
-        wait (dht_io);
-        #30000;
+        wait (dht11);
+        #30000; // 겹치는 구간 z 잠깐 발생할 수도 있음
         // 입력 모드로 변환
         io_oe = 1;
+        btn_R = 1;
+        #1_000_000;
+        btn_R = 0;
         dht_sensor_data = 1'b0;
         #80000;
         dht_sensor_data = 1'b1;
